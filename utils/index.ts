573a3a4020db6c50f14d539e7c046215d16227be
Luci <22126563+LuciNyan@users.noreply.github.com>
2024-02-21 15:58:14 +0800

    chore: add eslint

--- utils/index.ts
+++ utils/index.ts
@@ -5,23 +5,27 @@ export const hasMessage = objectOf({
 })
 
 export const isPATError = objectOf({
-  response: optional(objectOf({
-    data: optional(objectOf({
-      message: optional(isString)
-    }))
-  }))
+  response: optional(
+    objectOf({
+      data: optional(
+        objectOf({
+          message: optional(isString)
+        })
+      )
+    })
+  )
 })
 
-const ONE_MINUTE = 60;
-const FIVE_MINUTES = 300;
-const TEN_MINUTES = 600;
-const FIFTEEN_MINUTES = 900;
-const THIRTY_MINUTES = 1800;
-const TWO_HOURS = 7200;
-const FOUR_HOURS = 14400;
-const SIX_HOURS = 21600;
-const EIGHT_HOURS = 28800;
-const ONE_DAY = 86400;
+const ONE_MINUTE = 60
+const FIVE_MINUTES = 300
+const TEN_MINUTES = 600
+const FIFTEEN_MINUTES = 900
+const THIRTY_MINUTES = 1800
+const TWO_HOURS = 7200
+const FOUR_HOURS = 14400
+const SIX_HOURS = 21600
+const EIGHT_HOURS = 28800
+const ONE_DAY = 86400
 
 export const CONSTANTS = {
   ONE_MINUTE,
@@ -35,14 +39,15 @@ export const CONSTANTS = {
   EIGHT_HOURS,
   ONE_DAY,
   CARD_CACHE_SECONDS: SIX_HOURS,
-  ERROR_CACHE_SECONDS: TEN_MINUTES,
-};
+  ERROR_CACHE_SECONDS: TEN_MINUTES
+}
 
 export function dateDiff(d1: number | string | Date, d2: number | string | Date) {
-  const date1 = new Date(d1);
-  const date2 = new Date(d2);
-  const diff = date1.getTime() - date2.getTime();
-  return Math.round(diff / (1000 * 60));
+  const date1 = new Date(d1)
+  const date2 = new Date(d2)
+  const diff = date1.getTime() - date2.getTime()
+
+  return Math.round(diff / (1000 * 60))
 }
 
 export function parseArray(str: string | string[]): string[] {
@@ -51,20 +56,21 @@ export function parseArray(str: string | string[]): string[] {
   }
 
   if (!str) {
-    return [];
+    return []
   }
-  return str.split(',');
+
+  return str.split(',')
 }
 
 export function parseBoolean(value: string | string[]): boolean | undefined {
   if (!isString(value)) {
-    return undefined;
+    return undefined
   }
 
   if (value.toLowerCase() === 'true') {
-    return true;
+    return true
   } else if (value.toLowerCase() === 'false') {
-    return false;
+    return false
   }
 }
 

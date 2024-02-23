078383c40e9d47a8879b203daecceca259d11880
Luci <22126563+LuciNyan@users.noreply.github.com>
2024-02-23 19:28:47 +0800

    feat: enhance utils

--- packages/pixel-profile/src/utils/math.ts
+++ packages/pixel-profile/src/utils/math.ts
@@ -14,14 +14,14 @@ export function add2(a: Vec2, b: Vec2): Vec2 {
   return [a[0] + b[0], a[1] + b[1]]
 }
 
+export function subtract2(a: Vec2, b: Vec2): Vec2 {
+  return [a[0] - b[0], a[1] - b[1]]
+}
+
 export function dot2(a: Vec2, b: Vec2): number {
   return a[0] * b[0] + a[1] * b[1]
 }
 
 export function prod2(v: Vec2): number {
   return v[0] * v[1]
-}
-
-export function subtract2(vec1: Vec2, vec2: Vec2): Vec2 {
-  return [vec1[0] - vec2[0], vec1[1] - vec2[1]]
 }

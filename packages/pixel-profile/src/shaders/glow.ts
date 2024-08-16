5985d7513e601a2009b7f6ee4558d19d2702a950
Luci <22126563+LuciNyan@users.noreply.github.com>
2024-08-17 01:03:09 +0800

    chore: temp 1

--- packages/pixel-profile/src/shaders/glow.ts
+++ packages/pixel-profile/src/shaders/glow.ts
@@ -1,49 +1,77 @@
-import { render } from '../renderer'
+import { coordsToIndex, render, RGBA } from '../renderer'
 import { add2, Vec2 } from '../utils'
 import { add3, divide3, dot3, mix3, Vec3 } from '../utils/math'
 
-const radius = 10
-const intensity = 1
-const threshold = 0.9
-const _threshold = threshold * 255
+const radius = 5
+const intensity = 0.7
+const threshold = 0.8
 
 export function glow(source: Buffer, width: number, height: number): Buffer {
+  const memo: { result: boolean; sampledColor: RGBA; luminance: number }[] = new Array(width * height)
+
   return render(
     source,
     width,
     height,
-    (coords, texture) => {
-      const samplerColor = texture(coords)
+    (uv, texture2D) => {
+      const originalColor = texture2D(uv)
 
-      let bloomColor: Vec3 = [0, 0, 0]
+      let glowColor: Vec3 = [0, 0, 0]
       let n = 0
 
       for (let i = -radius; i <= radius; i++) {
         for (let j = -radius; j <= radius; j++) {
-          const offset: Vec2 = [i / width, j / height]
-          const sampledColor = texture(add2(coords, offset))
-          const luminance = dot3(sampledColor, [0.2126, 0.7152, 0.0722])
-          if (luminance > _threshold) {
-            bloomColor = add3(bloomColor, sampledColor)
+          const _uv: Vec2 = add2(uv, [i, j])
+          const key = coordsToIndex(_uv[0], _uv[1], width)
+          if (memo[key]) {
+            if (memo[key].result) {
+              const sampledColor = memo[key].sampledColor
+              const luminance = memo[key].luminance
+
+              const dist = length2([i, j])
+              const weight = Math.pow(Math.exp((-dist * dist) / (2 * radius * radius)), 3)
+              glowColor = add3(
+                glowColor,
+                mix3(originalColor, sampledColor, weight * intensity * Math.pow(luminance, 5))
+              )
+
+              n++
+            }
+            continue
+          }
+          memo[key] = {} as any
+
+          const sampledColor = texture2D(_uv)
+          const luminance = dot3(sampledColor, [0.2126, 0.7152, 0.0722]) / 255
+          if (luminance > threshold) {
+            const dist = length2([i, j])
+            const weight = Math.pow(Math.exp((-dist * dist) / (2 * radius * radius)), 3)
+            glowColor = add3(glowColor, mix3(originalColor, sampledColor, weight * intensity * Math.pow(luminance, 5)))
             n++
+
+            memo[key].result = true
+            memo[key].sampledColor = sampledColor
+            memo[key].luminance = luminance
+          } else {
+            memo[key].result = false
           }
         }
       }
 
       if (n === 0) {
-        return samplerColor
+        return originalColor
       }
 
-      bloomColor = divide3(bloomColor, n)
+      glowColor = divide3(glowColor, n)
 
-      const _bloomIntensity = intensity * (n / Math.pow(radius * 2 + 1, 2))
-
-      const finalColor = mix3(samplerColor, bloomColor, _bloomIntensity)
-
-      return [finalColor[0], finalColor[1], finalColor[2], 255]
+      return [glowColor[0], glowColor[1], glowColor[2], 255]
     },
     {
       textureFilter: 'NEAREST'
     }
   )
+}
+
+function length2(v: Vec2): number {
+  return Math.sqrt(v[0] * v[0] + v[1] * v[1])
 }
